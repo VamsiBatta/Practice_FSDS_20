ecec6378a86e64d4a7000b1db8005808b5be0bac
Vamsi_Batta <86851071+VamsiBatta@users.noreply.github.com>
2022-01-06 16:16:24 +0530

    Create Length_of_the_string.py

---
+++ Length_of_the_string.py
@@ -0,0 +1,9 @@
+def len_s(s):
+    """This Method Will return the Length of a String"""
+    count = 0
+    for i in s:
+        count += 1 
+    return count
+        
+a = "Vamsi"
+print(len_s(a))
