4ff8b320ea288a96f66a6f0406e8868168611ccd
Vamsi_Batta <86851071+VamsiBatta@users.noreply.github.com>
2022-01-06 17:52:12 +0530

    Create Index_of_Evey_Element.py
    It will Return the indexes of Every Element in the list

---
+++ Index_of_Evey_Element.py
@@ -0,0 +1,11 @@
+def index(l):
+    """This function prints the indexes of every element in a list even if the list contain the repetitive elements"""
+    Values = []
+    Index = []
+    for i in range(len(l)):
+        print("Index of ",l[i],"is ", i)
+        
+
+
+lst = [5,0,7,3,0,6,2,7]
+print(index(lst))
